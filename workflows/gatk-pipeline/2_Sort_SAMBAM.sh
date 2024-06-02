ae7956c58c0bac56b1374aa954ccd8aad38fc820
Rund Tawfiq <107937921+rund0wn@users.noreply.github.com>
2024-06-02 14:51:08 +0300

    Update 2_Sort_SAMBAM.sh

--- workflows/gatk-pipeline/2_Sort_SAMBAM.sh
+++ workflows/gatk-pipeline/2_Sort_SAMBAM.sh
@@ -5,8 +5,8 @@ SAMPLE="SampleName"
 
 # Sort BAM file
 picard -Xmx100g SortSam \
-I=$SAMPLE.bam \
-O=$SAMPLE.sort.bam \
+I=${SAMPLE}.bam \
+O=${SAMPLE}.sort.bam \
 VALIDATION_STRINGENCY=LENIENT \
 SORT_ORDER=coordinate \
 MAX_RECORDS_IN_RAM= 3000000 \
