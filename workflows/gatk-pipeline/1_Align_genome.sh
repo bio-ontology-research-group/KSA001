60218a1988f493fcc2843e284fe820b6eb90fa49
Rund Tawfiq <107937921+rund0wn@users.noreply.github.com>
2024-06-02 14:42:12 +0300

    Create 1_Align_genome.sh

---
+++ workflows/gatk-pipeline/1_Align_genome.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Set variables
+REFERENCE="ksa001.fa"
+READ1="Read1_R1.fastq.gz"
+READ2="Read2_R2.fastq.gz"
+SAMPLE="SampleName"
+
+# Index reference genome
+bwa index $REFERENCE
+
+# Align reads to indexed reference
+bwa mem -M -t 16 \
+-R "@RG\tID:HJJY3DRXY.1\tSM:t2t\tLB:Illumina_wgs\tPL:ILLUMINA" \
+$REFERENCE \
+$READ1 \
+$READ2 | \
+samtools view -b -h -o $SAMPLE.bam -
