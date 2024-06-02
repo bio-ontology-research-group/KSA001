04913d07513f3b13e7fb735bfe169f1d0d2e3938
Rund Tawfiq <107937921+rund0wn@users.noreply.github.com>
2024-06-02 14:43:58 +0300

    Create 2_Sort_SAMBAM.sh

---
+++ workflows/gatk-pipeline/2_Sort_SAMBAM.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Set variables
+SAMPLE="SampleName"
+
+# Sort BAM file
+picard -Xmx100g SortSam \
+I=$SAMPLE.bam \
+O=$SAMPLE.sort.bam \
+VALIDATION_STRINGENCY=LENIENT \
+SORT_ORDER=coordinate \
+MAX_RECORDS_IN_RAM= 3000000 \
+CREATE_INDEX=True
